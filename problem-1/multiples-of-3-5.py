12de49b6df1d02c8b79c2640e06b96d1a7df27ca
GuardianLux <unholyagent@gmail.com>
2020-05-03 00:23:33 +1000

    finished problem 2

--- problem-1/multiples-of-3-5.py
+++ problem-1/multiples-of-3-5.py
@@ -1,7 +1,8 @@
-sum = 0
-i = 0
-for i in range(1000):
-    if i%3 == 0 or i%5 == 0:
-        sum += i
-    i += 1
-print("The sum of all multiples of 3 & 5 below 1000 are: " + str(sum))
+def multiples_of_3_5():
+    sum = 0
+    i = 0
+    for i in range(1000):
+        if i%3 == 0 or i%5 == 0:
+            sum += i
+        i += 1
+    print("The sum of all multiples of 3 & 5 below 1000 are: " + str(sum))
---
+++ problem-2/even-fibonacci.py
@@ -0,0 +1,8 @@
+def even_fibonacci_sum():
+    a, b = 1, 2
+    while a < 4000000:
+        if a % 2 == 0:
+            yield a
+        a, b = b, a + b
+
+print(sum(even_fibonacci_sum()))
