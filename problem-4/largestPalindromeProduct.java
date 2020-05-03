7c4d852c1d1f2bfef90c13dda386f053eaf6083a
GuardianLux <unholyagent@gmail.com>
2020-05-03 20:38:31 +1000

    work on problem

--- problem-4/largestPalindromeProduct.java
+++ problem-4/largestPalindromeProduct.java
@@ -1,5 +1,27 @@
-public class Palindrome {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class largestPalindromeProduct {
     public static void main(String[] args) {
-        
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        ArrayList<Integer> palindromes = new ArrayList<Integer>();
+        while (a < 999) {
+            c = a + b;
+            if (String.valueOf(c) == String.valueOf(reverseNumber(c))) {
+                palindromes.add(c);
+            }
+        }
+        System.out.println(Collections.max(palindromes));
+    }
+    public static int reverseNumber(int i) {
+        int rev = 0;
+        while(i != 0) {
+            int digit = i % 10;
+            rev = rev * 10 + digit;
+            i /= 10;
+         }
+        return rev;
     }
 }
